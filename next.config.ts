3797a8da6a366402921a394ac3eaff5e233485e3
Le Hoang Trong <langtupro0456@gmail.com>
2024-11-10 23:02:52 +0700

    Fix tiếp tục lỗi package json

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,32 @@
-import NextBundleAnalyzer from '@next/bundle-analyzer'
-import createNextIntlPlugin from 'next-intl/plugin'
-import type { NextConfig } from 'next'
+import NextBundleAnalyzer from '@next/bundle-analyzer';
+import createNextIntlPlugin from 'next-intl/plugin';
+import type { NextConfig } from 'next';
+import path from 'path';
 
-const withNextIntl = createNextIntlPlugin()
+const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         hostname: 'localhost',
-        pathname: '/**'
+        pathname: '/**',
       },
       {
         hostname: 'api-bigboy.duthanhduoc.com',
-        pathname: '/**'
+        pathname: '/**',
       },
       {
         hostname: 'via.placeholder.com',
-        pathname: '/**'
-      }
-    ]
-  }
-}
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, './src');
+    return config;
+  },
+};
 const withBundleAnalyzer = NextBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true'
-})
-export default withBundleAnalyzer(withNextIntl(nextConfig))
+  enabled: process.env.ANALYZE === 'true',
+});
+export default withBundleAnalyzer(withNextIntl(nextConfig));
